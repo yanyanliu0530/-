c9278ee313d74cc85eccf49262471516528e6bf1
liuyan <yanyanliu0530@163.com>
2019-06-18 23:16:30 +0800

    nav

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function Index() {
+
+}
+Index.prototype = {
+    init: function () {
+        this.adve = document.querySelector
+    },
+    //广告位
+    adv: function () {
+
+    }
+}
+new Index()
